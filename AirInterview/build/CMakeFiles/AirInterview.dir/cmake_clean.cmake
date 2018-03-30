d633f805aa49036ae9bd8b5ffb47230e1f6d0e6e
fersolerv <36763559+fersolerv@users.noreply.github.com>
2018-03-30 11:23:26 +0000

    Add files via upload

---
+++ AirInterview/build/CMakeFiles/AirInterview.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AirInterview.dir/src/FindCylinder.cpp.o"
+  "AirInterview.pdb"
+  "AirInterview"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirInterview.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
